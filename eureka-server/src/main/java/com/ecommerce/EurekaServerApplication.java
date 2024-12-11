b87a9aace97e86149ef3e234cb677b2354a122c4
tharun-19 <chtharun973@gmail.com>
2024-12-11 16:17:36 +0530

    commit check

--- eureka-server/src/main/java/com/ecommerce/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/ecommerce/EurekaServerApplication.java
@@ -12,7 +12,7 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
-		System.out.println("Eureka has Server started");
+		System.out.println("Eureka Server started_____");
 	}
 
 }
